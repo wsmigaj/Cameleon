07d2ceca4b24d5105871bd353a07e91f4b03a7f4
Wojciech Śmigaj <w.smigaj@gmail.com>
2024-01-07 10:30:11 +0100

    Configure precompiled headers with CMake.

--- Document.cpp
+++ Document.cpp
@@ -1,6 +1,6 @@
 // This file is part of Caméléon.
 //
-// Copyright (C) 2023 Wojciech Śmigaj
+// Copyright (C) 2023-2024 Wojciech Śmigaj
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "stdafx.h"
 #include "Document.h"
 #include "PatternMatching.h"
 #include "RuntimeError.h"
